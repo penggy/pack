c8b2464354d6909dc39c3cafab815afc393993d1
penggy <peng.wu@foxmail.com>
2018-02-06 11:41:26 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# @penggy/pack
+
+pack your nodejs project
+
+把你的 `nodejs` 工程打成压缩文件, 通过配置, 可以控制最终生成的压缩文件中是否包含 `node_modules` 目录
+
+## install
+
+```shell
+npm i -g @penggy/pack
+```
+
+## usage
+
+```shell
+  Usage: pack [options] [command]
+
+
+  Options:
+
+    -V, --version           output the version number
+    -F, --format [tar,zip]  archive format [tar] (default: tar)
+    -h, --help              output usage information
+
+
+  Commands:
+
+    clean
+
+```
+
+## config
+
+pack 命令从当前工作目录找 package.json 文件, 根据 package.json 配置内容, 生成压缩文件.
+
+默认情况下, 打包除 node_moudle, *.log, *.tar.gz, *.zip 以外的所有文件(非点字符打头), 对应的 glob 配置如下:
+
+```js
+{
+    "pattern": "**",
+    "options": {
+      "dot": false,
+      "ignore": ["node_modules/**", "*.log", "*.tar.gz", "*.zip"]
+    }    
+}
+```
+
+如果默认配置不满足你的要求, 你也可以在 package.json 文件中自定义打包规则, 如:
+
+```js
+  "pack": {
+    "pattern": "**",
+    "options": {
+      "dot": false,
+      "ignore": ["*.log", "*.tar.gz", "*.zip"]
+    }
+  }
+```
+
+`pack` 字段还可以写成数组的形式, 以满足多条打包规则的情况
+
+`pack` 使用 `node-glob` 匹配文件, 更多详细的匹配说明, 请参考 [node-glob github](https://github.com/isaacs/node-glob)
+
+祝使用愉快!
+
---
+++ bin/pack.js
@@ -0,0 +1,110 @@
+#!/usr/bin/env node
+
+const archiver = require('archiver');
+const prettyBytes = require('pretty-bytes');
+const os = require('os');
+const path = require('path');
+const fs = require('fs-extra');
+const program = require('commander');
+const glob = require('glob');
+const moment = require('moment');
+process.title = "pack";
+
+var packFile = path.resolve(process.cwd(), "package.json");
+console.log(`work directory: ${process.cwd()}`);
+if (!fs.existsSync(packFile)) {
+    console.log('package.json not found.')
+    process.exit();
+}
+const package = require(packFile);
+var buildTime = moment().format('YYMMDDHHmm');
+var archiveName = `${package.name}-${package.version}-${buildTime}`;
+archiveName = archiveName.replace(/\//g,"-");
+
+function doArchive() {
+    switch (program.format) {
+        case 'zip':
+            var output = fs.createWriteStream(path.resolve(process.cwd(), `${archiveName}.zip`));
+            var archive = archiver('zip', { zlib: { level: 9 } })
+            break;
+        case 'tar':
+            var output = fs.createWriteStream(path.resolve(process.cwd(), `${archiveName}.tar.gz`));
+            var archive = archiver('tar', { gzip: true });
+            break;
+        default:
+            console.log('unknow format');
+            program.help();
+            break;
+    }
+    output.on('close', function () {
+        console.log(os.EOL + prettyBytes(archive.pointer()) + ' total bytes');
+    });
+    archive.on('warning', function (err) {
+        if (err.code === 'ENOENT') {
+            console.log(err);
+        } else {
+            throw err;
+        }
+    }).on('error', function (err) {
+        throw err;
+    }).on('progress', data => {
+        process.stdout.clearLine();
+        process.stdout.cursorTo(0);
+        process.stdout.write(`${prettyBytes(data.fs.processedBytes)} / ${prettyBytes(data.fs.totalBytes)}`);
+    })
+
+    archive.pipe(output);
+    var globs = [];
+    globs = globs.concat(package.pack || []);
+    if(!globs.length) {
+        globs.push({
+            pattern: "**",
+            options: {
+                dot: false,
+                ignore: ["node_modules{,/**}", "*.log", "*.tar.gz", "*.zip"]
+            }
+        })
+    }
+    for(var _glob of globs) {
+        archive.glob(_glob.pattern, Object.assign({}, _glob.options, {
+            cwd: process.cwd()
+        }), {
+            prefix: archiveName
+        })
+    }
+    console.log(`do ${program.format} ...`);
+    archive.finalize();
+}
+
+program.version(require(path.resolve(__dirname, "../package.json")).version)
+    .option('-F, --format [tar,zip]', 'archive format [tar]', 'tar')
+
+program.command('clean').action(function () {
+    var files = glob.sync("{*.zip,*.tar.gz}", {
+        cwd: process.cwd(),
+        dot: true,
+        absolute: true
+    })
+    for (file of files) {
+        console.log(`remove file [${file}]`);
+        fs.removeSync(file);
+    }
+    console.log('clean done.');
+    process.exit();
+})
+
+program.command('*', '', {
+    noHelp: true
+}).action(function () {
+    program.help();
+})
+
+program.parse(process.argv);
+
+doArchive();
+
+
+
+
+
+
